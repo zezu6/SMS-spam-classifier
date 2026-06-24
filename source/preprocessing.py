9b90887e4e9fe8da23c21f6fb8591ec8606984d4
Maciej Strzelczyk <zezucode@gmail.com>
2026-06-24 23:45:18 +0200

    TF-IDF and NB refactored into a pipeline

--- source/preprocessing.py
+++ source/preprocessing.py
@@ -1,9 +1,3 @@
-# import pandas as pd
-#
-# df = pd.read_csv(filepath_or_buffer="../data/spam.csv", encoding="latin-1")
-# df = df[["v1", "v2"]]
-# df.rename(columns={"v1": "output", "v2": "input"}, inplace=True)
-
 # set of allowed characters
 allowed = set([chr(i) for i in range(97, 123)] + [str(i) for i in range(10)] + [" "])
 
@@ -13,7 +7,3 @@ def clean(text: str) -> str:
     text = "".join(char for char in text if char in allowed)    # filtering characters
     text = " ".join(text.split())   # space normalization
     return text
-
-
-# df["input"] = df["input"].apply(clean)
-# print(df.tail())
--- source/train.py
+++ source/train.py
@@ -4,14 +4,13 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix, classification_report
-
+from sklearn.pipeline import Pipeline
 
 df = pd.read_csv(filepath_or_buffer="../data/spam.csv", encoding="latin-1")
 df = df[["v1", "v2"]]
 df.rename(columns={"v1": "output", "v2": "input"}, inplace=True)
 
 df["input"] = df["input"].apply(clean)
-
 X = df["input"]
 y = df["output"]
 
@@ -19,15 +18,13 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=10, stratify=y
 )
 
-vectorizer = TfidfVectorizer(stop_words="english", min_df=2)
+pipe = Pipeline(steps=[
+    ("vectorizer", TfidfVectorizer(stop_words="english", min_df=2)),
+    ("classifier", MultinomialNB())
+])
 
-X_train_vec = vectorizer.fit_transform(X_train)
-X_test_vec = vectorizer.transform(X_test)
-
-model = MultinomialNB()
-model.fit(X=X_train_vec, y=y_train)
-
-y_pred = model.predict(X=X_test_vec)
+pipe.fit(X_train, y_train)
+y_pred = pipe.predict(X_test)
 
 report = classification_report(y_true=y_test, y_pred=y_pred)
 conf_matrix = confusion_matrix(y_true=y_test, y_pred=y_pred)
@@ -36,7 +33,4 @@ print(f"report:\n{report}")
 print(f"confusion matrix:\n{conf_matrix}")
 
 
-#TODO: 1. Pipeline (TF-IDF + NB) + joblib
-#TODO: 2. CLI or FastAPI endpoint
-#TODO: 3. requirements and README
-#TODO: 4. Analise mistakes (FP/FN + confidence)
+#TODO: ClI + confidence + README,  requirements
